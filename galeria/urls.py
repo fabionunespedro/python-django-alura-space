e5246287a320280e7a67cb6760f7d29a3f497816
Fabio Nunes <fabio.pedro@live.com>
2025-08-02 19:48:39 -0300

    feat: gelaria-template finish

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-import include
+from galeria.views import index, imagem
 
 urlpatterns = [
-  path('', include('galeria.urls')),
+  path('', index, name= 'index'),
+  path('imagem/', imagem, name='imagem'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return HttpResponse('<h1>Alura Space</h1>')
+  return render(request, 'galeria/index.html')
+
+def imagem(request):
+  return render(request, 'galeria/imagem.html')
